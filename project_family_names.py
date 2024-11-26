b1aecf42b954ab70af9127fb5548d4795c8d8188
Jonskut <jonatantevaniemi@outlook.com>
2024-11-26 12:35:52 +0200

    imports for ml

--- project_family_names.py
+++ project_family_names.py
@@ -9,4 +9,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import librosa as lr
-
+import keras as kr
+import tensorflow as tf
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
