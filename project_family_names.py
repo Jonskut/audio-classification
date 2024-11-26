e52667eb31e02ee2135b2bcdb2c6b480ffd01bae
Jonskut <jonatantevaniemi@outlook.com>
2024-11-26 12:26:18 +0200

    testi

--- project_family_names.py
+++ project_family_names.py
@@ -1,5 +1,12 @@
 """
-Jonatan Tevaniemi
-Pelto Tian
+Jonatan Tevaniemi 150176680
+Pelto Tian 150180676
 Audio processing project
 """
+
+# Importing libraries for audio processing
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import librosa as lr
+
